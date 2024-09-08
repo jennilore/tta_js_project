2ed77356909beb735a922c59db90e85631e87e41
jennilore <jennilorkama@gmail.com>
2024-09-08 20:35:41 +0200

    feat successfully added the functionto add and categorize task

--- version3.js
+++ version3.js
@@ -8,9 +8,28 @@ function displayCategorizedTasks(){
     tasks.forEach((category) =>{
         console.log(`\t ${category[0]} tasks:`);
         category[1].forEach((tasks,index) =>{
-            console.log(`\t\t ${index + 1}. ${task}`);
+            console.log(`\t\t ${index + 1}. ${tasks}`);
         })
     })
 }
 
-displayCategorizedTasks()
+
+function addCategorizedTasks(category, task){
+    for (let i = 0; i < tasks.length; i++){
+        if (tasks[i][0] === category){
+            tasks[i][1].push(tasks);
+            console.log(`task "${task}" added to ${category} task \n`);
+            console.log("\n to do list after adding a new task \n");
+            return;
+        }
+    }
+    console.log(`category "${category}" not found \n`)
+
+}
+
+console.log("To do list before adding a new task \n");
+displayCategorizedTasks();
+
+
+addCategorizedTasks ('works', 'complete the report')
+displayCategorizedTasks();
