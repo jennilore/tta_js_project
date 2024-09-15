f75b7502287c6d0f4d2776ed1b8841435fb33e12
jennilore <jennilorkama@gmail.com>
2024-09-15 23:07:52 +0200

    ft understand n intro factory function n their propeerties

--- week12/objects.js
+++ week12/objects.js
@@ -11,7 +11,7 @@ function creatTask(title, description, category){
         },
 
 
-       displayDeatails(){
+        displayDeatails(){
             console.log("*...........................................*");
             console.log(`\t categoty; ${this.category} `)
             console.log(`\t task; ${this.title}`)
@@ -22,12 +22,41 @@ function creatTask(title, description, category){
             else{
                 console.log("\t task is not complteted");
             }
+        },
+
+        updateTitle(newTitle){
+            this.title = newTitle;
+            console.log("task title updated successfully")
+        },
+
+        updateDescription(newDescription){
+            this.description = newDescription;
+            console.log("Task description updated sucessfully")
+        },
+
+        updateCategory(newCategory){
+            this.category = newCategory
+            console.log("Tas category updated successfully")
         }
+        
     }
 }
 
-let task1 = creatTask("buy milk", "get milk, bread and buy eggs", "go shopping")
+let task1 = creatTask("buy milk", "get milk, bread and buy eggs", "go shopping");
+task1.displayDeatails();
 
 task1.toggleCompleted();
 console.log('|\t New task detail \t|');
+task1.displayDeatails();
+
+task1.updateTitle("buy groceries - 1");
+console.log('|\t Updated task detail \t|');
+task1.displayDeatails();
+
+task1 = updateDescription("1. buy milk", "get milk, bread and buy eggs"); 
+console.log('|\t Updated task detail \t|');
+task1.displayDeatails();
+
+task1 = updateCategory("shopping - 1"); 
+console.log('|\t Updated task detail \t|');
 task1.displayDeatails();
