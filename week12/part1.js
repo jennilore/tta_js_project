5acdf5f42480b965dc8bc9da29d4bf2d69bf91bc
jennilore <jennilorkama@gmail.com>
2024-09-15 22:02:59 +0200

    ft undunrstanding properties and methods

--- week12/part1.js
+++ week12/part1.js
@@ -20,17 +20,37 @@ class task{
         console.log(`\t categoty; ${this.category} `)
         console.log(`\t task; ${this.title}`)
         console.log(`\t description; ${this.description}`)
-        // console.log(`\t completed; ${this.completed}`)
-        // if (this.completed){
-        //     console.log("\t task is completed");
-        // }
-        // else{
-        //     console.log("\t task is not complteted");
-        // }
-
-        
+        console.log(`\t completed; ${this.completed}`)
+        if (this.completed){
+            console.log("\t task is completed");
+        }
+        else{
+            console.log("\t task is not complteted");
+        }
 
         console.log("*............................................*")
+    }
+
+    // updateTask(newTitle, newCategoty, newDescription){
+    //     this.title = newTitle;
+    //     this.category = newCategoty;
+    //     this.descriptionc = newDescription;
+    //     console.log("Task updated successfully");
+    // }
+
+    updateTaskTitle(newTitle){
+        this.title = newTitle
+        console.log("Task title updated sucessfully")
+    }
+
+    updateTaskDescription(newDescription){
+        this.description = newDescription
+        console.log("Task description updated sucessfully")
+    }
+
+    updateTaskCategory(newCategoty){
+        this.category = newCategoty
+        console.log("Task category updated sucessfully")
     }
 }
 
@@ -40,6 +60,9 @@ let task1 = new task ("get groceries","buy milk, bread, egg and jam in the store
 
 task1.displayDeatails();
 
+task1.updateTask("buy groceries - 1", "1. Get milk, bread, and eggs from the store");
+console.log('|\t Updated task detail \t|');
+task1.displayDeatails();
 
 task1.toggleCompleted();
 console.log('|\t New task detail \t|')
